201f147053c633117d6bd02a4da889d2881efae1
Стас Басов <stas.basov@whatever.com>
2023-01-10 20:51:11 +0800

    3_review_fix

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -49,9 +49,4 @@ class Post(models.Model):
         verbose_name_plural = 'Записи'
 
     def __str__(self):
-        return f'{format}'(
-            self.author.username,
-            self.pub_date,
-            self.group,
-            self.text
-        )
+        return f'{self.author.username}-{self.pub_date}-{self.group}-{self.text}'
